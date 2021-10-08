33b1cb4bc93f50c0399395b5a18b83acfc32f95c
lchang@expertise.com <lchang@expertise.com>
2021-10-08 16:49:56 +0000

    finishing up practice exercise (making total payment field)

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,1 +1,8 @@
-select order_id,customer_id,amount from {{ref('stg_orders')}}
+with orders as (
+    select * from {{ref('stg_orders')}}
+),
+    payments as (
+        select * from {{ref('stg_payments')}}
+    )
+
+select order_id,customer_id,amount from orders left join payments where payments.status = 'success'
